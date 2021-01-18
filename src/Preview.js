342dda4120b1fb30b50d8faa827dd5630638f5e9
Yeonsu <banananjuseyo@gmail.com>
2021-01-19 00:32:09 +0900

    upload pixelize img_230

--- src/Preview.js
+++ src/Preview.js
@@ -4,188 +4,188 @@ export const Preview = {
     desc: "Slide & Click motion & Canvas",
     responsive: true,
     img:
-      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master//src/img/pixel/fullSlide.jpg",
+      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master/src/img/pixel/fullSlide.jpg",
   },
   12: {
     name: "DailyMain",
     desc: "Detect Time, location & Use localStorage",
     responsive: true,
     img:
-      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master//src/img/pixel/dailyMain.jpg",
+      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master/src/img/pixel/dailyMain.jpg",
   },
   13: {
     name: "B : PicChangedByList",
     desc: "Slide & Click motion",
     responsive: true,
     img:
-      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master//src/img/pixel/picChangedByList.jpg",
+      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master/src/img/pixel/picChangedByList.jpg",
   },
   14: {
     name: "Blend Background",
     desc: "Page Changing Motion & Canvas Animation",
     responsive: true,
     img:
-      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master//src/img/pixel/blendBackground.jpg",
+      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master/src/img/pixel/blendBackground.jpg",
   },
   15: {
     name: "Pop Corner",
     desc: "Page Changing Motion & Pointer Animation",
     responsive: true,
     img:
-      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master//src/img/pixel/popCorner.jpg",
+      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master/src/img/pixel/popCorner.jpg",
   },
   16: {
     name: "UnC : BookShelf",
     desc: "Slide & Pointer Animation",
     responsive: true,
     img:
-      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master//src/img/pixel/fullSlide.jpg",
+      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master/src/img/pixel/fullSlide.jpg",
   },
   17: {
     name: "SideHideMenu",
     desc: "Responsive Touch Motion & Swipe on mobile",
     responsive: true,
     img:
-      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master//src/img/pixel/sideHideMenu.jpg",
+      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master/src/img/pixel/sideHideMenu.jpg",
   },
   18: {
     name: "BlockList",
     desc: "Tilt Motion",
     responsive: true,
     img:
-      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master//src/img/pixel/blockList.jpg",
+      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master/src/img/pixel/blockList.jpg",
   },
   19: {
     name: "EnteringBox",
     desc: "Scroll Interaction",
     responsive: true,
     img:
-      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master//src/img/pixel/enteringBox.jpg",
+      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master/src/img/pixel/enteringBox.jpg",
   },
   21: {
     name: "ColorBubble",
     desc: "Canvas Animation",
     responsive: true,
     img:
-      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master//src/img/pixel/colorBubble.jpg",
+      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master/src/img/pixel/colorBubble.jpg",
   },
   22: {
     name: "BottomBlur",
     desc: "Slide Animation & Click motion",
     responsive: true,
     img:
-      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master//src/img/pixel/bottomBlur.jpg",
+      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master/src/img/pixel/bottomBlur.jpg",
   },
   23: {
     name: "Grider",
     desc: "Page Changing Motion & Swipe on mobile",
     responsive: true,
     img:
-      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master//src/img/pixel/grider.jpg",
+      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master/src/img/pixel/grider.jpg",
   },
   24: {
     name: "AddParts",
     desc: "Scroll Animation & Scroll Menu",
     responsive: true,
     img:
-      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master//src/img/pixel/addParts.jpg",
+      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master/src/img/pixel/addParts.jpg",
   },
   25: {
     name: "SwitchProduct",
     desc: "Slide & Swipe on mobile",
     responsive: true,
     img:
-      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master//src/img/pixel/switchProduct.jpg",
+      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master/src/img/pixel/switchProduct.jpg",
   },
   26: {
     name: "LoopSlide",
     desc: "Loop Slide & Swipe on mobile",
     responsive: true,
     img:
-      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master//src/img/pixel/loopSlide.jpg",
+      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master/src/img/pixel/loopSlide.jpg",
   },
   27: {
     name: "CursorChaser",
     desc: "Type Interaction & Pointer Animation",
     responsive: true,
     img:
-      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master//src/img/pixel/cursorChaser.jpg",
+      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master/src/img/pixel/cursorChaser.jpg",
   },
   28: {
     name: "BarList",
     desc: "Page Changing Motion",
     responsive: true,
     img:
-      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master//src/img/pixel/barList.jpg",
+      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master/src/img/pixel/barList.jpg",
   },
   29: {
     name: "DragBox",
     desc: "Swipe Animation",
     responsive: true,
     img:
-      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master//src/img/pixel/dragBox.jpg",
+      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master/src/img/pixel/dragBox.jpg",
   },
   31: {
     name: "VerticalSwipe",
     desc: "keypad (Up Down) motion<br />& Swipe on max-width 900px",
     responsive: true,
     img:
-      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master//src/img/pixel/verticalSwipe.jpg",
+      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master/src/img/pixel/verticalSwipe.jpg",
   },
   32: {
     name: "AutoBGandGauge",
     desc: "Autochange Slide & Gauge keyframe",
     responsive: true,
     img:
-      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master//src/img/pixel/autoBGandGauge.jpg",
+      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master/src/img/pixel/autoBGandGauge.jpg",
   },
   33: {
     name: "Blind",
     desc: "Intro Animation",
     responsive: true,
     img:
-      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master//src/img/pixel/blind.jpg",
+      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master/src/img/pixel/blind.jpg",
   },
   34: {
     name: "CircleSlide",
     desc: "Page Changing Motion",
     responsive: true,
     img:
-      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master//src/img/pixel/circleSlide.jpg",
+      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master/src/img/pixel/circleSlide.jpg",
   },
   35: {
     name: "TimeLine",
     desc: "Slide & Swipe on mobile",
     responsive: true,
     img:
-      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master//src/img/pixel/timeLIne.jpg",
+      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master/src/img/pixel/timeLIne.jpg",
   },
   36: {
     name: "Gallery",
     desc: "List & Modal & Parallex",
     responsive: true,
     img:
-      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master//src/img/pixel/gallery.jpg",
+      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master/src/img/pixel/gallery.jpg",
   },
   37: {
     name: "ModalInfo",
     desc: "modal",
     responsive: true,
     img:
-      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master//src/img/pixel/modalInfo.jpg",
+      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master/src/img/pixel/modalInfo.jpg",
   },
   38: {
     name: "YellowCard",
     desc: "Scroll Interaction",
     responsive: true,
     img:
-      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master//src/img/pixel/yellowCard.jpg",
+      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master/src/img/pixel/yellowCard.jpg",
   },
   39: {
     name: "tripAdviser",
     desc: "fullPage",
     responsive: true,
     img:
-      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master//src/img/pixel/tripAdviser.jpg",
+      "https://raw.githubusercontent.com/nyangmeonggood/thekingofmains/master/src/img/pixel/tripAdviser.jpg",
   },
 };
